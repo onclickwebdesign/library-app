abf25c9b79c55aff04f34b5a0258b7d74a65ec2b
Eric Atallah <eric.atallah@onclickwebdesign.com>
2019-07-21 00:30:17 -0700

    Add foreach in books template.

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 
-/*
-let retrieveBooksSql = 
+const retrieveBooksSql = 
     `
     SELECT 
         book.id,
@@ -19,54 +18,22 @@ let retrieveBooksSql =
     INNER JOIN book_location ON book.book_location_id = book_location.id
     INNER JOIN book_language ON book.book_language_id = book_language.id
     `;
-*/
 
 
 router.get('/', async (req, res) => {
-    const sql = 
-        `
-        SELECT 
-            book.id,
-            book.author,
-            book.title,
-            book_type.type,
-            book_sub_type.sub_type,
-            book_location.location,
-            book_language.language
-        FROM book
-        INNER JOIN book_type ON book.book_type_id = book_type.id
-        INNER JOIN book_sub_type ON book.book_sub_type_id = book_sub_type.id
-        INNER JOIN book_location ON book.book_location_id = book_location.id
-        INNER JOIN book_language ON book.book_language_id = book_language.id;
-        `;
+    const sql = `${retrieveBooksSql} LIMIT 30;`;
     
-
-    // ********** start with one hard coded book to show how data flows to hbs templates *************
-    const book = {
-        id: 1,
-        author: 'Miguel Cervantes',
-        title: 'Don Quixote',
-        type: 'Fiction',
-        sub_type: 'Novel',
-        location: 'Office',
-        language: 'English'
-    };
-
-    res.render('books', { book });
-
-    /*
     let err;
     const books = await pool.query(sql).catch(e => err = e);
     if (err) {
         res.render('books', { books: [], errorMsg: 'There was an error retrieving your books, please reload this page.' });
     }
-    res.render('books', { books: books });
-    */
+    res.render('books', { books });
 });
 
 // Search books by query parameter string
 router.get('/searchbooks', (req, res) => {
-    res.render('books', { message: 'In /searchbooks route.' });
+    res.render('books');
 });
 
 // Get book info (Google Books API)
